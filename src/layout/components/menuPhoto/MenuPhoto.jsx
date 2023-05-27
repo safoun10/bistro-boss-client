ea77b59d82349141613aef76fbd9398ef3712639
Safoun <safoun.official@gmail.com>
2023-05-27 09:05:14 +0600

    made photo text responsive

--- src/layout/components/menuPhoto/MenuPhoto.jsx
+++ src/layout/components/menuPhoto/MenuPhoto.jsx
@@ -3,7 +3,7 @@ import "swiper/css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/bundle";
 import { Pagination } from "swiper";
-import "./MenuPhoto.css"
+import "./MenuPhoto.css";
 
 const MenuPhoto = () => {
 	return (
@@ -18,59 +18,83 @@ const MenuPhoto = () => {
 			>
 				<SwiperSlide className="img-fluid">
 					<img
-						className="rounded-4 mb-5 mt-2 responsive-img-menu"
+						className="rounded-4 mt-2 responsive-img-menu"
 						src="https://i.postimg.cc/90YG6hwQ/slide1.jpg"
 						alt="photo"
 					/>
+					<div className="text-in-photo text-center text-fancy">
+						salads
+					</div>
 				</SwiperSlide>
 				<SwiperSlide className="img-fluid">
 					<img
-						className="rounded-4 mb-5 mt-2 responsive-img-menu"
+						className="rounded-4 mt-2 responsive-img-menu"
 						src="https://i.postimg.cc/8z5h0xpT/slide2.jpg"
 						alt="photo"
 					/>
+					<div className="text-in-photo text-center text-fancy">
+						salads
+					</div>
 				</SwiperSlide>
 				<SwiperSlide className="img-fluid">
 					<img
-						className="rounded-4 mb-5 mt-2 responsive-img-menu"
+						className="rounded-4 mt-2 responsive-img-menu"
 						src="https://i.postimg.cc/fT5c5MhH/slide3.jpg"
 						alt="photo"
 					/>
+					<div className="text-in-photo text-center text-fancy">
+						salads
+					</div>
 				</SwiperSlide>
 				<SwiperSlide className="img-fluid">
 					<img
-						className="rounded-4 mb-5 mt-2 responsive-img-menu"
+						className="rounded-4 mt-2 responsive-img-menu"
 						src="https://i.postimg.cc/bwBQcLrk/slide4.jpg"
 						alt="photo"
 					/>
+					<div className="text-in-photo text-center text-fancy">
+						salads
+					</div>
 				</SwiperSlide>
 				<SwiperSlide className="img-fluid">
 					<img
-						className="rounded-4 mb-5 mt-2 responsive-img-menu"
+						className="rounded-4 mt-2 responsive-img-menu"
 						src="https://i.postimg.cc/90YG6hwQ/slide1.jpg"
 						alt="photo"
 					/>
+					<div className="text-in-photo text-center text-fancy">
+						salads
+					</div>
 				</SwiperSlide>
 				<SwiperSlide className="img-fluid">
 					<img
-						className="rounded-4 mb-5 mt-2 responsive-img-menu"
+						className="rounded-4 mt-2 responsive-img-menu"
 						src="https://i.postimg.cc/8z5h0xpT/slide2.jpg"
 						alt="photo"
 					/>
+					<div className="text-in-photo text-center text-fancy">
+						salads
+					</div>
 				</SwiperSlide>
 				<SwiperSlide className="img-fluid">
 					<img
-						className="rounded-4 mb-5 mt-2 responsive-img-menu"
+						className="rounded-4 mt-2 responsive-img-menu"
 						src="https://i.postimg.cc/fT5c5MhH/slide3.jpg"
 						alt="photo"
 					/>
+					<div className="text-in-photo text-center text-fancy">
+						salads
+					</div>
 				</SwiperSlide>
 				<SwiperSlide className="img-fluid">
 					<img
-						className="rounded-4 mb-5 mt-2 responsive-img-menu"
+						className="rounded-4 mt-2 responsive-img-menu"
 						src="https://i.postimg.cc/bwBQcLrk/slide4.jpg"
 						alt="photo"
 					/>
+					<div className="text-in-photo text-center text-fancy">
+						salads
+					</div>
 				</SwiperSlide>
 			</Swiper>
 		</div>

c6afbe240b4f1322c6fcebb23d515f4dadb75b07
Safoun <safoun.official@gmail.com>
2023-05-27 08:47:49 +0600

    solved some responsiveness issues

--- src/layout/components/miniTitle/MiniTitle.jsx
+++ src/layout/components/miniTitle/MiniTitle.jsx
@@ -5,9 +5,9 @@ const MiniTitle = ({ time, text }) => {
 	return (
 		<div className="w-50 mini-title-container mx-auto">
 			<div className="text-center text-gold d-flex justify-content-center align-items-center">
-				-----------
+				<span className="text-responsive">-------------</span>
 				<span className="px-2 fst-italic py-3 fs-5">{time}</span>
-				-----------
+				<span className="text-responsive">-------------</span>
 			</div>
 			<div className="text-title text-center text-uppercase display-4 py-3 text-fancy">
 				{text}

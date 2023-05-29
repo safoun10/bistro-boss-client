44af0f7bae536a412f1d873e62874d88ff6b5436
Safoun <safoun.official@gmail.com>
2023-05-29 18:51:44 +0600

    added the chef service section

---
+++ src/layout/components/chef-service/ChefService.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./ChefService.css"
+
+const ChefService = () => {
+    return (
+		<div className="bg-photo-chef-service">
+			<div className="bg-white py-5 bg-opacity-75">
+				<div className="text-center text-fancy display-2 mb-3">
+					bistro Boss
+				</div>
+				<div className="w-75 mx-auto text-center">
+					Bistro Boss: Where Culinary Mastery Reigns Supreme.
+					Experience an exquisite fusion of flavors and sophistication
+					at Bistro Boss, a culinary haven that captivates discerning
+					palates. Indulge in meticulously crafted dishes, blending
+					French culinary techniques with global inspirations. Immerse
+					yourself in an inviting ambiance, complemented by impeccable
+					service, as Bistro Boss takes you on an unforgettable
+					gastronomic journey
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ChefService;
--- src/layout/pages/home/Home.jsx
+++ src/layout/pages/home/Home.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import Banner from '../../components/banner/Banner';
 import OrderOnline from '../../components/orderOnline/OrderOnline';
 import MenuPhoto from '../../components/menuPhoto/MenuPhoto';
+import ChefService from '../../components/chef-service/ChefService';
+import MiniTitle from '../../components/miniTitle/MiniTitle';
 
 const Home = () => {
     return (
 		<div>
 			<Banner></Banner>
 			<OrderOnline></OrderOnline>
-			<div className='mx-auto' style={{maxWidth : "1200px"}}>
+			<div className="mx-auto" style={{ maxWidth: "1200px" }}>
 				<MenuPhoto></MenuPhoto>
+			</div>
+			<div className="mx-auto mb-5" style={{ maxWidth: "1200px" }}>
+				<ChefService></ChefService>
+			</div>
+			<div className='mb-5'>
+				<MiniTitle time="Check it out" text="From our menu"></MiniTitle>
 			</div>
 		</div>
 	);
